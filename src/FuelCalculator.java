4d1b32147c9c585bb2a4495ebfeecac82fe77af9
Dylan Friendly <friendly.dylan@gmail.com>
2019-12-28 00:53:45 -0600

    Made Day 01, part 01 look better. Finished rest of day 01

--- src/FuelCalculator.java
+++ src/FuelCalculator.java
@@ -1,34 +1,51 @@
-//Advent of Code 2019, Day 01
+// Advent of Code 2019, Day 01
 public class FuelCalculator {
-  
-  public static int calcRequiredFuel(int mass) {
+
+  // For First Challenge
+  public static int calcRequiredFuelByInitialMass(int[] masses) {
     int fuel = 0;
-    // Fuel per mass calculation. mass divided by three and rounded down. Then subtract two.
-    fuel = (mass / 3) - 2;
+    for (int i = 0; i < masses.length; i++) {
+      // Fuel per mass calculation. mass divided by three and rounded down. Then subtract two.
+      fuel += (masses[i] / 3) - 2;
+    }
     return fuel;
   }
 
-  public static int arrayAdd(int[] array) {
-    int totalFuel = 0;
-    for (int fuel : array) {
-      totalFuel += fuel;
+
+
+  // Second Challenge
+  public static int calcRequiredFuelHelper(int[] masses) {
+    int fuelNeeded = 0;
+    for (int i = 0; i < masses.length; i++) {
+      fuelNeeded += calcRequiredFuel(masses[i], 0);
     }
-    return totalFuel;
+    return fuelNeeded;
   }
 
-  public static void main(String[] args) {
-    // test
-    int[] test = {12, 14, 1969, 100756};
-    int[] expected = {2, 2, 654, 33583};
-    int[] testResult = new int[expected.length];
-    for (int i=0; i<expected.length;i++) {
-      testResult[i] = calcRequiredFuel(test[i]);
-      if(testResult[i]!=expected[i]) {
-        System.out.println("Test has failed");
-      }
+  public static int calcRequiredFuel(int mass, int fuelNeeded) {
+    while ((mass / 3) - 2 > 0) {
+      mass = (mass / 3) - 2;
+      fuelNeeded += mass;
     }
-    System.out.println("Test has passed");
-    
+    return fuelNeeded;
+  }
+
+
+  public static void main(String[] args) {
+    // test for part 01
+    int[] test = {12, 14, 1969, 100756};
+    int expected = 34241;
+    if (calcRequiredFuelByInitialMass(test) != expected) {
+      System.out.println("First test has failed");
+    } else {
+      System.out.println("First test has passed");
+    }
+    // test for part 02
+    if (calcRequiredFuelHelper(new int[] {100756, 14}) == 50348) {
+      System.out.println("Second Test passed");
+    } else {
+      System.out.println("Second Test failed");
+    }
 
     // solution
     int[] input = {129192, 58561, 57267, 95382, 84995, 127372, 93598, 97264, 138550, 79327, 135661,
@@ -40,11 +57,12 @@ public class FuelCalculator {
         124098, 110382, 121557, 103613, 133576, 122801, 112306, 120203, 134696, 76129, 84576, 80854,
         147237, 71025, 127513, 143631, 125090, 115698, 57979, 84880, 120177, 147389, 88380, 114688,
         56355, 126265, 58220, 63523, 130179};
-    int solution=0;
-    for(int i=0; i<input.length;i++) {
-      solution += calcRequiredFuel(input[i]);
-    }
-    System.out.println("Fuel needed: "+ solution);
+    int solution = 0;
+    solution= calcRequiredFuelByInitialMass(input);
+    System.out.println("Fuel needed by initial masses: " + solution);
+    System.out.println("Fuel needed in total: " + calcRequiredFuelHelper(input));
   }
+
+
 
 }
